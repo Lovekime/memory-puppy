fbf2c0383f52668a244c936d22ea3bdca5a02ecc
Lovekime <525017784@qq.com>
2026-07-20 11:11:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,66 @@
+const file =
+document.getElementById("zipFile");
 
+
+const fileName =
+document.getElementById("fileName");
+
+
+const start =
+document.getElementById("start");
+
+
+const status =
+document.getElementById("status");
+
+
+
+file.onchange = ()=>{
+
+
+if(file.files.length){
+
+
+fileName.innerHTML =
+"🐾 "+file.files[0].name;
+
+
+status.innerHTML =
+"收到啦！小狗准备好了 🐶";
+
+
+}
+
+
+};
+
+
+
+start.onclick = ()=>{
+
+
+if(!file.files.length){
+
+status.innerHTML =
+"🥺 先给小狗一个 ZIP 文件吧";
+
+return;
+
+}
+
+
+status.innerHTML =
+"🐶 正在拆开聊天包...";
+
+
+setTimeout(()=>{
+
+
+status.innerHTML =
+"✨ 下一步：开始解析聊天记录";
+
+
+},1500);
+
+
+};
